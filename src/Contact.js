2c500bb9958d51bcee8f46728858a4800d42ecef
Asma <as1588ma@gmail.com>
2022-08-25 21:10:47 -0400

    add contacts.js file

---
+++ src/Contact.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <div>
+      <div>
+        <b>Github:</b> https://github.com/Asmasa1588
+      </div>
+      <div>
+        <b>Linkedin:</b> https://www.linkedin.com/in/asmasadiq/
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
